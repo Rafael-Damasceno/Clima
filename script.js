77eff95c62d9773e454d20aae9c6899abcfaf17a
Rafael Damasceno <rafaeldamascenowhy@gmail.com>
2023-02-05 20:27:19 -0300

    add js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector('.busca').addEventListener('submit', (event) => {
+    event.preventDefault(); // previne o comportamento padrão
+
+    let input = document.querySelector('searchInput').value;
+
+    if(input !== ''){
+        
+    }
+
+
+})
+
