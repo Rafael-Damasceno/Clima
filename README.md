08541783686da2b241138f65e3e425bf131e5dc3
Rafael Damasceno <rafaeldamascenowhy@gmail.com>
2023-02-05 22:26:57 -0300

    edit Readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Clima
+> site teste de requisições e manipulação de API
+
+Projeto construído como exercício do curso da B7web
+> propício a mudanças 
+
+![Preview](./.github/Preview.png)
+
+[Clique aqui para acessar]()
+
+## Tecnologias:
+
+- HTML
+- CSS
+- Git e Github
+- javaScript
+
+### Aprendido:
+
+- Manipulação de API
+- Requisição interna
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
     let input = document.querySelector('#searchInput').value;
 
     if(input !== ''){
+        clearInfo();
         showWarning('carregando...')
 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=9041a63e1d9b241be76ec78de5452c76&units=metric&lang=pt_br`
@@ -11,28 +12,49 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
         let results = await fetch(url);
         let json = await results.json();
 
-        if(json.code === 200){
-
+        if(json.cod === 200){
             showInfo({
                 name: json.name,
-                country: json.sys.country 
+                country: json.sys.country,
+                temp: json.main.temp,
+                temp_icon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
 
             })
 
         }else{
-            showWarning('Não encontramos esta localização')
+            clearInfo();
+            showWarning('Não encontramos esta localização');
         }
+    }else{
+        clearInfo();
     }
 
 
 })
 
 function showInfo(json){
+    showWarning('');
+   
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `${json.temp}<sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed}<span>km/h</span>`;
 
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.temp_icon}@2x.png`);
+
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)` 
+    
+    document.querySelector('.resultado').style.display = 'block';
 }
 
 function showWarning(msg) {
     document.querySelector('.aviso').innerHTML = msg;
+}
+
+function clearInfo(){
+    showWarning('')
+    document.querySelector('.resultado').style.display = 'none';
 }
 
  
