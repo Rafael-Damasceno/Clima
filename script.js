ae560ad94337986ab23b38089246647352e6a168
Rafael Damasceno <rafaeldamascenowhy@gmail.com>
2023-02-05 21:41:21 -0300

    teste

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-document.querySelector('.busca').addEventListener('submit', (event) => {
+document.querySelector('.busca').addEventListener('submit', async (event) => {
     event.preventDefault(); // previne o comportamento padrão
 
-    let input = document.querySelector('searchInput').value;
+    let input = document.querySelector('#searchInput').value;
 
     if(input !== ''){
-        
+        showWarning('carregando...')
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=9041a63e1d9b241be76ec78de5452c76&units=metric&lang=pt_br`
+
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if(json.code === 200){
+
+            showInfo({
+                name: json.name,
+                country: json.sys.country 
+
+            })
+
+        }else{
+            showWarning('Não encontramos esta localização')
+        }
     }
 
 
 })
 
+function showInfo(json){
+
+}
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg;
+}
+
+ 
